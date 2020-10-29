4c812d1d2d3e930afa6b1681bcd090d571163cf3
Bochkarev Evgeny <bochkarev_1906@mail.ru>
2020-10-29 22:37:44 +0300

    Edited js files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const openWeatherAPI = new OpenWeatherAPI()
 const weatherHere = document.querySelector('.weather-here')
 const weatherCityFavorites = document.querySelector('.weather-city-list')
+if (!localStorage.getItem('favoritesList'))
+    localStorage.setItem('favoritesList', '[]')
 refreshWeatherHere()
+loadWeatherFavorites()
 
 const buttonUpdate = document.querySelectorAll('.update-button, .update-media')
-buttonUpdate[i].addEventListener('click', refreshWeatherHere)	
+for (let i = 0; i < buttonUpdate.length; i++){ 
+    if (buttonUpdate){
+        buttonUpdate[i].addEventListener('click', refreshWeatherHere)
+    }
+}	
 const buttonAdd = document.querySelector('.add-city')
 buttonAdd.addEventListener('submit', addToFavorites)
--- js/openWeatherAPI.js
+++ js/openWeatherAPI.js
@@ -2,13 +2,16 @@ class OpenWeatherAPI{
     constructor(){
         this.APIKey = 'c58e6bb780cae68578dd9ecad1db5756'
     }
-    getByNameOfCity(cityName){
-        const responce = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.APIKey}&lang=ru&units=metric`)
-        return responce.json()
+    getIcon(iconCode) {
+        return `http://openweathermap.org/img/wn/${iconCode}.png`
     }
-    getByCoordinatesOfCity(position){
+    async getByNameOfCity(cityName){
+        const responce = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName.split('_').join(' ')}&appid=${this.APIKey}&lang=ru&units=metric`)
+        return await responce.json()
+    }
+    async getByCoordinatesOfCity(position){
         const [latitude, longitude] = [position.coords.latitude, position.coords.longitude]
-        const responce = fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this.APIKey}&lang=ru&units=metric`)
-        return responce.json()
+        const responce = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this.APIKey}&lang=ru&units=metric`)
+        return await responce.json()
     }
 }
